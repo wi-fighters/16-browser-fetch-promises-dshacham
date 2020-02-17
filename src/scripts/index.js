646376506b5540e7498dc856a3142861fad5fbb5
Dana Shacham <dshacham22@gmail.com>
2020-02-17 15:39:13 +0100

    add finished task

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,4 +4,19 @@ import '../styles/main.scss';
 
 
 // \/ All of your javascript should go here \/
+const modal = document.getElementById('myModal');
+let modalPromise = new Promise(function (resolve) {
+    setTimeout(() => {
+        resolve(modal);
+    }, (1000 * 60));
+});
 
+modalPromise.then(function (modal) {
+    modal.style.display = 'block';
+});
+
+modal.addEventListener('click', (e) => {
+    if (e.target.className === 'close') {
+        modal.style.display = 'none';
+    };
+});
